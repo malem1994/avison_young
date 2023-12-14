b19ce942a2536235b841e215100357f660028dfa
Huy Nguyen <malem1992.hn@gmail.com>
2023-12-15 06:07:53 +0700

    update page 2

--- assets/js/origin.js
+++ assets/js/origin.js
@@ -1,11 +1,25 @@
-$(document).ready(function () {
-  const priceChange = $('#price-change');
-  const priceTextChange = $('#price-change .text');
-  const priceComptChange = $('#price-change-content .item');
+$(window).on("load", function () {
 
-  priceChange.on("click", function () {
-    priceTextChange.toggleClass("active");
-    priceComptChange.toggleClass("active");
-  }
-  )
-});
+
+  $("#header").load("/layout/header.html");
+  $("#footer").load("/layout/footer.html");
+  $("#filter").load("/components/filter.html", function (data) {
+    const priceChange = $('#price-change');
+    const priceTextChange = $('#price-change .text');
+    const priceComptChange = $('#price-change-content .item');
+
+    priceChange.on('click', function () {
+      priceTextChange.toggleClass('active');
+      priceComptChange.toggleClass('active');
+    });
+
+    $(".js-range-slider").ionRangeSlider({
+      type: "double",
+      grid: true,
+      max: 10000,
+      from: 100,
+      to: 3000,
+      prefix: "$"
+    });
+  });
+})
