864bb2de85a5a8b35be79bdfb09d71ea85637497
Huy Nguyen <malem1992.hn@gmail.com>
2023-12-15 17:09:37 +0700

    update

--- public/assets/js/origin.js
+++ public/assets/js/origin.js
@@ -1,45 +1,35 @@
 $(window).on("load", function () {
-  $("#header").load("/public/layout/header.html", function () {
-    const burger = $('#burger');
-    const menu = $('#navg');
 
-    burger.on('click', function () {
-      menu.toggleClass('active');
-    })
+  const priceChange = $('#price-change');
+  const priceTextChange = $('#price-change .text');
+  const priceComptChange = $('#price-change-content .item');
+
+  priceChange.on('click', function () {
+    priceTextChange.toggleClass('active');
+    priceComptChange.toggleClass('active');
   });
 
-  $("#footer").load("/public/layout/footer.html");
-  $("#sidebar").load("/public/layout/sidebar.html", function () {
-    const toggleSidebar = $('#sidebarToggle');
-    const conttentSidebar = $('.sidebar-content');
-
-    toggleSidebar.on('click', function () {
-      conttentSidebar.slideToggle();
-    })
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    grid: true,
+    max: 10000,
+    from: 100,
+    to: 3000,
+    prefix: "$"
   });
+  const burger = $('#burger');
+  const menu = $('#navg');
 
-  $("#filter").load("/public/components/filter.html", function () {
-    const priceChange = $('#price-change');
-    const priceTextChange = $('#price-change .text');
-    const priceComptChange = $('#price-change-content .item');
+  burger.on('click', function () {
+    menu.toggleClass('active');
+  })
 
-    priceChange.on('click', function () {
-      priceTextChange.toggleClass('active');
-      priceComptChange.toggleClass('active');
-    });
+  const toggleSidebar = $('#sidebarToggle');
+  const conttentSidebar = $('.sidebar-content');
 
-    $(".js-range-slider").ionRangeSlider({
-      type: "double",
-      grid: true,
-      max: 10000,
-      from: 100,
-      to: 3000,
-      prefix: "$"
-    });
-  });
-
-  $("#tab").load("/public/components/tab.html");
-  $("#bio").load("/public/components/bio.html");
+  toggleSidebar.on('click', function () {
+    conttentSidebar.slideToggle();
+  })
 
   AOS.init();
 })
